91170121bf1fd56c46f3b4eac632513866f601d6
JaxCavalera <mr_maniac_x@hotmail.com>
2019-03-03 11:25:42 +1000

    added support for ie11, upgraded deepClone to be more readable and potentially a little more robust in terms of max stack size, rigged up some prototype code using web workers for use with the new timers functionality

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,8 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent, useEffect } from 'react';
+
+// Web Workers
+import { WebWorker } from '../../utils/workers/WebWorker';
+import { animFrameTimeoutWorker } from '../../utils/workers/anim-frame-timeout';
 
 // Error Handlers
 import ErrorBoundary from '../../utils/ErrorBoundary';
@@ -15,6 +19,7 @@ import { StoreContext } from '../../rootReducer';
 
 // Logic
 import { handleAddToCartOnClick } from './ProductCard-logic';
+// import { animFrameTimeout } from '../../utils/workers/anim-frame-timeout';
 
 // ProductCard Props
 export interface ProductCardProps {
@@ -22,16 +27,31 @@ export interface ProductCardProps {
   storeContext: StoreContext;
 }
 
+const animFrameTimeout = WebWorker(animFrameTimeoutWorker);
+
 export const ProductCard: React.FC<ProductCardProps> = ({ data, storeContext }) => {
   const [selectedSize, updateSelectedSize] = useState(Object.keys(data.availableSizes)[0] || 'Sold Out');
+  const [someTxt, updateSomeTxt] = useState('empty');
 
   const handleSizePickerOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
     updateSelectedSize(e.currentTarget.value);
   };
 
   const callHandleAddToCartOnClick = () => {
+    animFrameTimeout.postMessage(data);
     handleAddToCartOnClick(storeContext, data, selectedSize);
   };
+
+  useEffect(() => {
+    animFrameTimeout.addEventListener('message', (event: MessageEvent) => {
+      console.log('inside the listener');
+      if (event.data.value === data.value) {
+        updateSomeTxt(event.data.msg);
+      }
+    })
+    // const bail = animFrameTimeout(() => { console.log('got called'); updateSomeTxt('delay over'); }, 3000);
+    // bail();
+  }, []);
 
   return (
     <ErrorBoundary>
@@ -39,6 +59,7 @@ export const ProductCard: React.FC<ProductCardProps> = ({ data, storeContext }) 
         <SectionParagraph nomargin={true}>{data.label}</SectionParagraph>
         <WrappedImage imgSrc={data.imgUrl || ''} imgHeight={'100%'} imgWidth={'100%'} />
         <SectionParagraph nomargin={true}>${data.price.toFixed(2)}</SectionParagraph>
+        <SectionParagraph>{someTxt}</SectionParagraph>
         <CardActions>
           <AddToCartBtn onClick={callHandleAddToCartOnClick}>Add to Cart</AddToCartBtn>
           <SizePicker onChange={handleSizePickerOnChange} value={selectedSize}>
---
+++ src/utils/workers/WebWorker.ts
@@ -0,0 +1,5 @@
+export const WebWorker = (worker: (...args: any) => any) => {
+  const code = worker.toString();
+  const blob = new Blob([`(${code})()`]);
+  return new Worker(URL.createObjectURL(blob));
+};
