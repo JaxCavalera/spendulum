bc7a5af53a5a71c3bd0027e16ef997e73c0b9059
JaxCavalera <mr_maniac_x@hotmail.com>
2019-03-16 13:13:02 +1000

    [issue-13] implemented code to remove items from the cartSidebar when the trash button is clicked or all claimed sizes have a qty of zero

--- src/components/CartItem/CartItem-logic.ts
+++ src/components/CartItem/CartItem-logic.ts
@@ -58,3 +58,58 @@ export const handleItemQtyOnChange = (
     });
   }
 };
+
+export const handleTrashBtnOnClick = (
+  cartItem: ProductInfo,
+  store: RootReducerStore,
+  dispatch: React.Dispatch<any>,
+  claimedSizesIsEmpty: boolean,
+) => {
+  const { cartItemMicroStoreIds } = store.cartSidebarStore;
+  const newCartItemMicroStoreIdList = cartItemMicroStoreIds.filter(microStoreId => microStoreId !== cartItem.value);
+
+  dispatch({
+    type: CartSidebarActionTypes.UPDATE_CART_ITEM_MICROSTORE_ID_LIST,
+    cartItemMicroStoreIds: newCartItemMicroStoreIdList,
+  });
+
+  if (claimedSizesIsEmpty) {
+    dispatch({
+      type: CartSidebarActionTypes.REMOVE_MICROSTORE,
+      cartItemMicroStoreId: cartItem.value,
+    });
+
+    return;
+  }
+
+  if (store.productListStore[cartItem.value]) {
+    // Item has claimedSizes that need to be returned before it can be removed from the cart
+    const adjustedProductData: ProductInfo = Object.keys(cartItem.claimedSizes).reduce(
+      (productData, claimedSize) => {
+        const claimedQty = cartItem.claimedSizes[claimedSize] || 0;
+        const newProductData = updateProductSizes(productData, claimedSize, claimedQty, false);
+        return newProductData || productData;
+      },
+      store.productListStore[cartItem.value],
+    );
+
+    // Return claimedSizes to product
+    dispatch({
+      type: ProductListActionTypes.ASSIGN_MICROSTORE,
+      productMicroStoreId: cartItem.value,
+      productData: adjustedProductData,
+    });
+
+    // Remove cartItem microStore
+    dispatch({
+      type: CartSidebarActionTypes.REMOVE_MICROSTORE,
+      cartItemMicroStoreId: cartItem.value,
+    });
+
+    return;
+  }
+
+  // No matching productMicroStore was found for a cartItem (should be impossible)
+  console.error('A matching product does not exist for an item in the cartSidebar!');
+  return;
+};
